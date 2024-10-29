09e89a5205659b91fb3a29e62055f9977b250533
Juan Ignacio <juan.lorca@uc.cl>
2024-10-29 14:57:21 -0300

    IP changes

--- Jetson/src/client/params.py
+++ Jetson/src/client/params.py
@@ -1,4 +1,4 @@
 import os 
 
-PLANT_SERVER_IP = os.getenv("PLANT_SERVER_IP", "opc.tcp://localhost:4848")
+PLANT_SERVER_IP = os.getenv("PLANT_SERVER_IP", "opc.tcp://192.168.0.122:4840")
 TS_PLANT = int(os.getenv("TS_PLANT", 1))
